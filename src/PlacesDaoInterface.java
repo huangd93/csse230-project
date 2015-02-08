e326ccc75e46571d76cf4ae8869e2f8d446238de
huangd93 <huangd@rose-hulman.edu>
2015-02-08 17:12:35 -0500

    Blah
    Stuff

--- src/PlacesDaoInterface.java
+++ src/PlacesDaoInterface.java
@@ -3,7 +3,18 @@ import java.util.ArrayList;
 
 public interface PlacesDaoInterface {
 	
+	/**
+	 * Returns an ArrayList of all places in the Dao
+	 * @return
+	 */
 	public ArrayList<Place> getPlaces();
+	
+	/**
+	 * Finds what realm the input place belongs in
+	 * @param place
+	 * @return Realm 
+	 */
+	public Realm findPlace(Place place);
 	
 	
 }
