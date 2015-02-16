23c12105c121ef7affdc6c957742ee39db03f25e
LadyMopar <richarel@rose-hulman.edu>
2015-02-16 11:18:43 -0500

    PlacesDao Testing
    created a class in which to test PlacesDao

---
+++ src/PlacesDaoTesting.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+
+public class PlacesDaoTesting {
+	
+	@Test
+	private void getPlacesTest(){
+		
+	}
+	
+	@Test
+	private void getFastestRouteTest(){
+		
+	}
+	
+	@Test
+	private void getShortestRouteTest(){
+		
+	}
+	
+	@Test
+	private void insertIntoRatingTreeTest(){
+		
+	}
+
+}
