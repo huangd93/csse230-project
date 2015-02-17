7ac63e301d1c7eb59d89bf50f1d4860de1253025
LadyMopar <richarel@rose-hulman.edu>
2015-02-17 11:06:34 -0500

    Test Cases Update
    We're getting null pointer exceptions and infinite loops :(

--- src/PlacesDaoTesting.java
+++ src/PlacesDaoTesting.java
@@ -1,26 +1,70 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
-
 public class PlacesDaoTesting {
-	
+
+	Creation yggdrasil = new Creation();
+
+	public PlacesDaoTesting() {
+		yggdrasil.createAsgard();
+		yggdrasil.createAlfheim();
+		yggdrasil.createJotunheim(); // aka Utgard
+		yggdrasil.createMidgard();
+		yggdrasil.createMuspelheim();
+		yggdrasil.createNidavellir();
+		yggdrasil.createNiflheim();
+		yggdrasil.createSvartalfheim();
+		yggdrasil.createVanaheim();
+	}
+
 	@Test
-	private void getPlacesTest(){
+	public void getPlacesTest() {
+		assertEquals(41, yggdrasil.t.getPlaces().size()); // makes sure it gets
+															// all the places
+		assertEquals(5, yggdrasil.asgardPlaces.size());
+		assertEquals(3, yggdrasil.muspelheimPlaces.size());
+	}
+
+	@Test
+	public void getFastestRouteTest() {
+		assertEquals(
+				2,
+				yggdrasil.t.getFastestRoute(
+						yggdrasil.t.getPlace("Valhalla", "Asgard"),
+						yggdrasil.t.getPlace("Sea Of Marmora", "Asgard"))
+						.size());
+//		assertEquals(
+//				4,
+//				yggdrasil.t.getFastestRoute(
+//						yggdrasil.t.getPlace("Valhalla", "Asgard"),
+//						yggdrasil.t.getPlace("The Mountain Thrymheim",
+//								"Jotunheim")).size());
 
 	}
-	
+
 	@Test
-	private void getFastestRouteTest(){
-		
+	public void getShortestRouteTest() {
+		assertEquals(
+				1,
+				yggdrasil.t.getShortestRoute(
+						yggdrasil.t.getPlace("Valhalla", "Asgard"),
+						yggdrasil.t.getPlace("Odin's Fortress", "Asgard"))
+						.size());
+//		assertEquals(
+//				4,
+//				yggdrasil.t.getFastestRoute(
+//						yggdrasil.t.getPlace("Valhalla", "Asgard"),
+//						yggdrasil.t.getPlace("The Mountain Thrymheim",
+//								"Jotunheim")).size());
+
 	}
-	
+
 	@Test
-	private void getShortestRouteTest(){
-		
-	}
-	
-	@Test
-	private void insertIntoRatingTreeTest(){
-		
+	public void insertIntoRatingTreeTest() {
+
 	}
 
 }
